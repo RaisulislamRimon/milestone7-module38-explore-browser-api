1517f0829656b571ab67fae0fd001de8ec2f6682
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2022-09-17 07:14:25 +0000

    update

---
+++ module38-1/1 BOM.js
@@ -0,0 +1,58 @@
+const statusActivity = "active";
+const namePerson = "Mango";
+const age = 2;
+const mood = "happy";
+const color = "yellow";
+
+console.log(222);
+console.log(statusActivity);
+console.log(namePerson);
+console.log(age);
+console.log(mood);
+console.log(color);
+
+// document.getElementById("btn-search");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
